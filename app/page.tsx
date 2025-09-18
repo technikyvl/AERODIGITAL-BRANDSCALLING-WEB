e52419526773e36af71ffbdb0fc5ba16f969dd3a
technikyvl <jakub4.biznes@gmail.com>
2025-09-18 12:37:05 +0200

    cofnięcie zmian

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { GlobeDemo } from '@/components/ui/globe-demo'
-
-export default function Home() {
-  return (
-    <main className="min-h-screen bg-black flex items-center justify-center p-8">
-      <GlobeDemo />
-    </main>
-  )
-}
